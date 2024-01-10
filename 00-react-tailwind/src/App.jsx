deb801cb2d5b8f08ce73fa4ed7acab596fd55d50
novojitdas <novojitdas@gmail.com>
2024-01-10 16:38:52 +0600

    react & tailwind template added

---
+++ 00-react-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl text-blue-500">Welcome To React Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
